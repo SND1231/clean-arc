ecb8b74675d794158c76dca41308fbaabfa17ad2
Sanada.Y <villa7iniesta8@gmail.com>
2023-02-03 12:30:53 +0900

    テスト用のsql作成

---
+++ schema/test/insert.sql
@@ -0,0 +1,4 @@
+INSERT INTO workers (worker_id, name, status)  VALUES ("test", "taro", 0)
+
+INSERT INTO auth (worker_id, password, status, change_password_count, fail_auth_count)  
+VALUES ("test", "aaa", 1, 0, 0)
