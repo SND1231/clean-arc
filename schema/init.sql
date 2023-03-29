f606b0131c2e2731a10f5a10bb4b6a9807d076da
Sanada.Y <villa7iniesta8@gmail.com>
2023-03-29 09:52:23 +0900

    fix:モデルの変更

--- schema/init.sql
+++ schema/init.sql
@@ -1,7 +1,8 @@
 CREATE TABLE `workers` (
   `worker_id` varchar(255) NOT NULL,
+  `email` varchar(255)  NOT NULL DEFAULT "",
   `name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL,
-  `status` int(1) NOT NULL COMMENT '0:非活性, 1:活性',
+  `status` int(1) NOT NULL COMMENT '0:削除, 1:利用可能, 2:一時停止',
   `created_at` datetime NOT NULL,
   `updated_at` datetime NOT NULL,
   PRIMARY KEY (`worker_id`)
@@ -13,7 +14,6 @@ CREATE TABLE `auth` (
   `session` varchar(255)  NOT NULL DEFAULT "",
   `change_password_count` int(1) NOT NULL,
   `fail_auth_count` int(1) NOT NULL,
-  `status` int(1) NOT NULL COMMENT '0:削除, 1:利用可能, 2:一時停止',
   `created_at` datetime NOT NULL,
   `updated_at` datetime NOT NULL,
   foreign key (`worker_id`)  references workers(`worker_id`),
